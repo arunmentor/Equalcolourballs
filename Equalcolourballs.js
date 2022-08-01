aa6b507c08c2085ca7618cda4de326dbb1cc5de6
rarunrajan <80462952+rarunrajan@users.noreply.github.com>
2022-08-01 22:44:05 +0530

    Add files via upload

---
+++ Equalcolourballs.js
@@ -0,0 +1,11 @@
+
+function Equalcolourballs(array) { 
+
+  // code goes here  
+  return result; 
+
+}
+   
+// keep this function call here 
+console.log("Test scenario result"+Equalcolourballs(array));
+
